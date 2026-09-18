f8f6885667267220b83d68937f3f4ca457ca20bf
Amit4411 <136713887+Amit4411@users.noreply.github.com>
2026-09-18 23:11:13 +0530

    Add PlayerStatus enum for ActivityAFK states

---
+++ src/main/java/com/corruptedstudio/activityafk/player/PlayerStatus.java
@@ -0,0 +1,11 @@
+package com.corruptedstudio.activityafk.player;
+
+/**
+ * The current AFK or activity state for a player.
+ */
+public enum PlayerStatus {
+    ACTIVE,
+    AFK_MANUAL,
+    AFK_AUTOMATIC,
+    AFK_ZONE
+}
